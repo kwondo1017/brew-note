6efd0f920208d2e43f5e45b599efbd5e4d32f02a
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-02-17 17:01:26 +0900

    Fix: beantastinglog command remove beanId

--- src/main/java/org/travelslog/brewnote/bean/Entity/command/BeanTastingLogUpdateCommand.java
+++ src/main/java/org/travelslog/brewnote/bean/Entity/command/BeanTastingLogUpdateCommand.java
@@ -3,7 +3,6 @@ package org.travelslog.brewnote.bean.entity.command;
 import java.time.LocalDate;
 
 public record BeanTastingLogUpdateCommand(
-    Long beanId,
     LocalDate tastingDate,
     Integer beanScore,
     String tastingNote) {
