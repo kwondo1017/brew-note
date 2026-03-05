7d64678f0bf5ab033f0200ac3a9b1d6a9506a551
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-05 13:13:52 +0900

    remove @setter

--- src/main/java/org/travelslog/brewnote/bean/Entity/Bean.java
+++ src/main/java/org/travelslog/brewnote/bean/Entity/Bean.java
@@ -17,12 +17,10 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "bean")
 @Getter
-@Setter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Bean {
     @Id
