e6a6ccc4b4b85c953f373e76defd8e6315b38d6d
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-05 14:30:01 +0900

    fix beantastinglogcupnote-relation column to joincolumn

--- src/main/java/org/travelslog/brewnote/bean/Entity/relation/BeanTastingLogCupNoteRelation.java
+++ src/main/java/org/travelslog/brewnote/bean/Entity/relation/BeanTastingLogCupNoteRelation.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,11 +25,11 @@ public class BeanTastingLogCupNoteRelation {
     private Long id;
 
     @ManyToOne
-    @Column(name = "bean_tasting_log_id", nullable = false)
+    @JoinColumn(name = "bean_tasting_log_id", nullable = false)
     private BeanTastingLog beanTastingLog; // NOT NULL
 
     @ManyToOne
-    @Column(name = "cup_note_id", nullable = false)
+    @JoinColumn(name = "cup_note_id", nullable = false)
     private CupNote cupNote; // NOT NULL
 
     @Column(name = "type", nullable = false)
