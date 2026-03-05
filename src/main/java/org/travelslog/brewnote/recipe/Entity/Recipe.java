55efa0df7617eb3c0ee27be9d73fa3826b32b4fb
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-05 14:45:04 +0900

    add recipe column name

--- src/main/java/org/travelslog/brewnote/recipe/Entity/Recipe.java
+++ src/main/java/org/travelslog/brewnote/recipe/Entity/Recipe.java
@@ -1,5 +1,6 @@
 package org.travelslog.brewnote.recipe.entity;
 
+import org.checkerframework.checker.units.qual.C;
 import org.travelslog.brewnote.recipe.BeverageType;
 
 import jakarta.persistence.Column;
@@ -21,18 +22,28 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "recipe_name", nullable = false)
     private String recipeName; // NOT NULL
 
+    @Column(name = "beverage_type")
     private BeverageType beverageType; // Hot, Iced
+
     private String dripper;
     private String filter;
     private String grinder;
+
+    @Column(name = "grind_setting")
     private int grindSetting; // 분쇄도
+
+    @Column(name = "recommend_roasting_point")
     private String recommendRoastingPoint; // 추천 로스팅 포인트
+    @Column(name = "coffee_amount")
     private int coffeeAmount; // 원두량 (g)
+    @Column(name = "water_amount")
     private int waterAmount; // 물량 (ml)
+    @Column(name = "water_temperature")
     private int waterTemperature; // 물 온도 (°C)
+    @Column(name = "recipe_url")
     private String recipeUrl; // 추출 레시피 URL
 
     @OneToMany(mappedBy = "recipeId")
