6d7bbfd4566da867b854f3146f703eccfd76a143
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-05 14:27:47 +0900

    fix relation FK column to joincolumn

--- src/main/java/org/travelslog/brewnote/bean/Entity/relation/BeanCupNoteRelation.java
+++ src/main/java/org/travelslog/brewnote/bean/Entity/relation/BeanCupNoteRelation.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,11 +25,11 @@ public class BeanCupNoteRelation {
     private Long id;
 
     @ManyToOne
-    @Column(name = "bean_id", nullable = false)
+    @JoinColumn(name = "bean_id", nullable = false)
     private Bean bean; // NOT NULL
 
     @ManyToOne
-    @Column(name = "cup_note_id", nullable = false)
+    @JoinColumn(name = "cup_note_id", nullable = false)
     private CupNote cupNote; // NOT NULL
 
     @Column(name = "type", nullable = false)
