2174989a362573df0b076c3a2e18af47eb4cfbf7
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-05 14:40:33 +0900

    add  insertable, updatable false

--- src/main/java/org/travelslog/brewnote/recipe/Entity/PouringStep.java
+++ src/main/java/org/travelslog/brewnote/recipe/Entity/PouringStep.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -27,6 +28,7 @@ public class PouringStep {
     private String stepTime; // NOT NULL
 
     @ManyToOne
+    @JoinColumn(name = "recipe_id", insertable = false, updatable = false)
     private Recipe recipe; // NOT NULL
 }
 
