d6ae9cd0b86100939391d0736e137bd1598effc8
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-12 14:32:40 +0900

    add cupnote table PROTECTED

--- src/main/java/org/travelslog/brewnote/cupnote/entity/CupNote.java
+++ src/main/java/org/travelslog/brewnote/cupnote/entity/CupNote.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @Table(name = "cup_note")
 public class CupNote {
     @Id
