e3937bae41460b3184c4b6ee7ee25575f9292d76
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-16 22:04:59 +0900

    add BrewnoteApplicationTests Disabled

--- src/test/java/org/travelslog/brewnote/BrewnoteApplicationTests.java
+++ src/test/java/org/travelslog/brewnote/BrewnoteApplicationTests.java
@@ -2,12 +2,13 @@ package org.travelslog.brewnote;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
 class BrewnoteApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
-
 }
