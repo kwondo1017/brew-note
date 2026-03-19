caf75bfdbe359f819c125f11d3fc41910c4f0977
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 14:20:04 +0900

    add CRUD on bean service

--- src/main/java/org/travelslog/brewnote/bean/service/BeanService.java
+++ src/main/java/org/travelslog/brewnote/bean/service/BeanService.java
@@ -3,6 +3,7 @@ package org.travelslog.brewnote.bean.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.travelslog.brewnote.bean.entity.Bean;
+import org.travelslog.brewnote.bean.entity.command.BeanUpdateCommand;
 import org.travelslog.brewnote.bean.repository.BeanRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,24 +15,31 @@ public class BeanService {
 
     private final BeanRepository beanRepository;
 
-    public Long create(String beanName) {
-        Bean bean = new Bean(beanName);
+    @Transactional
+    public Long createBean(BeanUpdateCommand command) {
+        Bean bean = new Bean(command.beanName());
         return beanRepository.save(bean).getId();
     }
 
     @Transactional(readOnly = true)
-    public Bean get(Long id) {
-
+    public Bean getBean(Long id) {
         return beanRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Bean not found"));
     }
 
-    public void update(Long id, String beanName, String roastery, Integer price, String purchaseUrl) {
-        Bean bean = get(id);
-        //bean.update(beanName, roastery, price, null, purchaseUrl, null, null, null, null, null, null, null, null, null);
+    @Transactional(readOnly = true)
+    public Iterable<Bean> getBeans() {
+        return beanRepository.findAll();
     }
 
-    public void delete(Long id) {
+    @Transactional
+    public void updateBean(Long id, BeanUpdateCommand command) {
+        Bean bean = getBean(id);
+        bean.apply(command);
+    }
+
+    @Transactional
+    public void deleteBean(Long id) {
         beanRepository.deleteById(id);
     }
 }   
