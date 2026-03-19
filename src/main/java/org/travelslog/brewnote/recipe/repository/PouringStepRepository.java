fac6fec53ea8f4de055016eac4182e272b14c1fc
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 11:48:15 +0900

    add search pouringStep orderBy step

--- src/main/java/org/travelslog/brewnote/recipe/repository/PouringStepRepository.java
+++ src/main/java/org/travelslog/brewnote/recipe/repository/PouringStepRepository.java
@@ -1,7 +1,10 @@
 package org.travelslog.brewnote.recipe.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.travelslog.brewnote.recipe.entity.PouringStep;
 
 public interface PouringStepRepository extends JpaRepository<PouringStep, Long> {
+    List<PouringStep> findByRecipeIdOrderByStepOrderIndexAsc(Long recipeId);
 }
