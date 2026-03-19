29cb7944ce662ef29f746ffd3d67bee85f05aa6b
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 13:30:23 +0900

    fix name beanId -> bean beantastingRepository

--- src/main/java/org/travelslog/brewnote/bean/repository/BeanTastingLogRepository.java
+++ src/main/java/org/travelslog/brewnote/bean/repository/BeanTastingLogRepository.java
@@ -1,9 +1,10 @@
 package org.travelslog.brewnote.bean.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.travelslog.brewnote.bean.entity.BeanTastingLog;
 
 public interface BeanTastingLogRepository extends JpaRepository<BeanTastingLog, Long> {
-    List<BeanTastingLog> findByBeanIdOrderByTastingDateDesc(Long beanId);
+    List<BeanTastingLog> findByBean_IdOrderByTastingDateDesc(Long beanId);
 }
