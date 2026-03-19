6e927f364ca6eb266afe0fffa0f8b27235370398
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 11:32:46 +0900

    add find beanId orderby tastingdate QueryMethod

--- src/main/java/org/travelslog/brewnote/bean/repository/BeanTastingLogRepository.java
+++ src/main/java/org/travelslog/brewnote/bean/repository/BeanTastingLogRepository.java
@@ -1,7 +1,9 @@
 package org.travelslog.brewnote.bean.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.travelslog.brewnote.bean.entity.BeanTastingLog;
 
 public interface BeanTastingLogRepository extends JpaRepository<BeanTastingLog, Long> {
+    List<BeanTastingLog> findByBeanIdOrderByTastingDateDesc(Long beanId);
 }
