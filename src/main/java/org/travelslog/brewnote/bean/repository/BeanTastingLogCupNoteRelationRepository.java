c7a89cce81e72e3d10417f278855cfa52f536a54
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 11:45:18 +0900

    add search cupnote connected beanTastingLogId

--- src/main/java/org/travelslog/brewnote/bean/repository/BeanTastingLogCupNoteRelationRepository.java
+++ src/main/java/org/travelslog/brewnote/bean/repository/BeanTastingLogCupNoteRelationRepository.java
@@ -1,7 +1,10 @@
 package org.travelslog.brewnote.bean.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.travelslog.brewnote.bean.entity.relation.BeanTastingLogCupNoteRelation;
 
 public interface BeanTastingLogCupNoteRelationRepository extends JpaRepository<BeanTastingLogCupNoteRelation, Long> {
+    List<BeanTastingLogCupNoteRelation> findByBeanTastingLogId(Long beanTastingLogId);
 }
