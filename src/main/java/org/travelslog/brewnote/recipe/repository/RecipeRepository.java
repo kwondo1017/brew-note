a508b747ddd6704424644fe206b60108cae5d699
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 11:47:11 +0900

    add search BeverageType recipe QueryMethod

--- src/main/java/org/travelslog/brewnote/recipe/repository/RecipeRepository.java
+++ src/main/java/org/travelslog/brewnote/recipe/repository/RecipeRepository.java
@@ -1,7 +1,11 @@
 package org.travelslog.brewnote.recipe.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.travelslog.brewnote.recipe.entity.BeverageType;
 import org.travelslog.brewnote.recipe.entity.Recipe;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByBeverageType(BeverageType beverageType);
 }
