3cd76df15216c53c2a7b71b46eed13611c359a97
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-03-19 09:14:16 +0900

    add beanRepository findBy QueryMethod

--- src/main/java/org/travelslog/brewnote/bean/repository/BeanRepository.java
+++ src/main/java/org/travelslog/brewnote/bean/repository/BeanRepository.java
@@ -1,7 +1,11 @@
 package org.travelslog.brewnote.bean.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.travelslog.brewnote.bean.entity.Bean;
 
 public interface BeanRepository extends JpaRepository<Bean, Long> {
+    List<Bean> findByBeanNameContainingIgnoreCase(String beanName);
+    List<Bean> findByRoasteryContainingIgnoreCase(String roastery);
+    List<Bean> findByCountryContainingIgnoreCase(String country);
 }
